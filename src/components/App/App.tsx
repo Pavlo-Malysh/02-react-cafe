270b5d07cfef84d1c53b936d6ecbf013c2167ebc
Pavlo Malysh <mal.pawlo@gmail.com>
2025-07-14 01:15:32 +0300

    add components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import CafeInfo from '../CafeInfo/CafeInfo'
+import type { Votes, VoteType } from '../../types/votes'
+import css from './App.module.css'
+import VoteOptions from '../VoteOptions/VoteOptions'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: votes[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    })
+
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
       </div>
-      <h1>Vite + React + TS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
