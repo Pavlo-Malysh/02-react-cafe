94f041113fb6c8d245b64773473604bee96347d4
Pavlo Malysh <mal.pawlo@gmail.com>
2025-07-14 20:58:47 +0300

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "modern-normalize";
-// Глобальні стилі (додатково)
-import "./global.css";
 import './index.css'
 import App from './components/App/App'
 
